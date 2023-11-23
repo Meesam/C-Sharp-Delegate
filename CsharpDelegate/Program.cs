6b2d29ca82babf62d7ca61fa2c539ae59e489006
meesam <meesam.engineer@gmail.com>
2023-11-23 08:15:05 +0530

    initial commit

---
+++ CsharpDelegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public delegate void VoidDelegate();
+
+    public delegate void VoidDelegateWithParameter(string msg);
+    
+    static void Main(string[] args)
+    {
+        VoidDelegate voidDelegate = Print1;
+        DemoDelegate.ExecuteDelegate(voidDelegate);
+        VoidDelegateWithParameter voidDelegateWithParameter = Print2;
+        DemoDelegateWithParam.ExecuteDelegateWithParam(voidDelegateWithParameter);
+    }
+
+    private static void Print1()
+    {
+        Console.WriteLine("Print1 method");
+    }
+    
+    private static void Print2(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+class DemoDelegate
+{
+    public static void ExecuteDelegate(Program.VoidDelegate voidDelegate)
+    {
+        voidDelegate();
+    }
+}
+
+class DemoDelegateWithParam
+{
+    public static void ExecuteDelegateWithParam(Program.VoidDelegateWithParameter voidDelegate)
+    {
+        voidDelegate("Delegate with Param");
+    }
+}
